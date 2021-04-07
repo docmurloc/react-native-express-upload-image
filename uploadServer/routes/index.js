01a9a803bbaf719c11d84900e2530bac82755382
pierre ANTOINE <pierre.ant7@hotmail.com>
2021-04-07 08:20:24 +0200

    setup server to upload image

--- uploadServer/routes/index.js
+++ uploadServer/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+const multer = require('multer');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './images');
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post('/upload', upload.array('photo', 3), (req, res) => {
+  console.log('file', req.files);
+  console.log('body', req.body);
+  res.status(200).json({
+    message: 'success!',
+  });
 });
 
 module.exports = router;
